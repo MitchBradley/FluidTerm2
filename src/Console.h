9d7dc12bc1c54d22e37c760511b3cdb4e7c89e45
Mitch Bradley <wmb@firmworks.com>
2026-01-02 13:37:18 -0800

    Added another common file

---
+++ src/Console.h
@@ -0,0 +1,10 @@
+#pragma once
+
+void editModeOn();
+void editModeOff();
+bool setConsoleModes();
+bool setConsoleColor();
+void restoreConsoleModes();
+int  getConsoleChar();
+void clearScreen();
+bool availConsoleChar();
